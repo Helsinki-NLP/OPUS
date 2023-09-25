3d109bc8085b42d97e75ccc6bb402f9a539a46a3
Lukas Weymann <62622663+lukasweymann@users.noreply.github.com>
2023-09-25 14:31:47 +0200

    Update news.js

--- info/news.js
+++ info/news.js
@@ -1,63 +1,63 @@
- [ {
-    title: `<a href="/MDN_Web_Docs/corpus/version/MDN_Web_Docs" >MDN_Web_Docs</a>`,
-    date: "2023-09-25",
+{ "newsList": {
+    "title": `<a href="/MDN_Web_Docs/corpus/version/MDN_Web_Docs" >MDN_Web_Docs</a>`,
+    "date": "2023-09-25",
   },
   {
-    title: `<a href="/NLLB/corpus/version/NLLB" >NLLB</a>`,
-    date: "2023-09-07",
+    "title": `<a href="/NLLB/corpus/version/NLLB" >NLLB</a>`,
+    "date": "2023-09-07",
   },
   {
-    title: `<a href="https://github.com/Helsinki-NLP/OPUS" target="_blank" rel="noreferrer">OPUS on GitHub</a>`,
-    date: "",
+    "title": `<a href="https://github.com/Helsinki-NLP/OPUS" target="_blank" rel="noreferrer">OPUS on GitHub</a>`,
+    "date": "",
   },
   {
-    title: `<a href="/liv4ever/corpus/version/liv4ever">Liv4ever</a> and <a href="/ELITR-ECA/corpus/version/ELITR-ECA">ELITR-ECA</a>`,
-    date: "2021-12-08",
+    "title": `<a href="/liv4ever/corpus/version/liv4ever">Liv4ever</a> and <a href="/ELITR-ECA/corpus/version/ELITR-ECA">ELITR-ECA</a>`,
+    "date": "2021-12-08",
   },
   {
-    title: `<a href="/CCMatrix/corpus/version/CCMatrix"> CCMatrix</a>`,
-    date: "2021-06-28",
+    "title": `<a href="/CCMatrix/corpus/version/CCMatrix"> CCMatrix</a>`,
+    "date": "2021-06-28",
   },
   {
-    title: `Updated: <a href="/ParaCrawl/corpus/version/ParaCrawl">ParaCrawl</a> and <a href="/MultiParaCrawl/corpus/version/MultiParaCrawl">MultiParaCrawl</a>`,
-    date: "2021-06-11",
+    "title": `Updated: <a href="/ParaCrawl/corpus/version/ParaCrawl">ParaCrawl</a> and <a href="/MultiParaCrawl/corpus/version/MultiParaCrawl">MultiParaCrawl</a>`,
+    "date": "2021-06-11",
   },
   {
-    title: `New: <a href="/MT560">MT560 dataset</a>`,
-    date: "2021-04-02",
+    "title": `New: <a href="/MT560">MT560 dataset</a>`,
+    "date": "2021-04-02",
   },
   {
-    title: `<a href="/CCAligned/corpus/version/CCAligned">CCAligned</a>  and <a href="/MultiCCAligned/corpus/version/MultiCCAligned">MultiCCAligned</a>`,
-    date: "2021-02-10",
+    "title": `<a href="/CCAligned/corpus/version/CCAligned">CCAligned</a>  and <a href="/MultiCCAligned/corpus/version/MultiCCAligned">MultiCCAligned</a>`,
+    "date": "2021-02-10",
   },
   {
-    title: `<a href="/GoURMET/corpus/version/GoURMET">GoURMET</a> and <a href="/MIZAN/corpus/versionMIZANT">MIZAN</a>`,
-    date: "2020-11-27",
+    "title": `<a href="/GoURMET/corpus/version/GoURMET">GoURMET</a> and <a href="/MIZAN/corpus/versionMIZANT">MIZAN</a>`,
+    "date": "2020-11-27",
   },
   {
-    title: `<a href="/EuroPat/corpus/version/EuroPat">EuroPat</a> and <a href="/tico-19/corpus/version/tico-19">tico-19</a>`,
-    date: "2020-10-31",
+    "title": `<a href="/EuroPat/corpus/version/EuroPat">EuroPat</a> and <a href="/tico-19/corpus/version/tico-19">tico-19</a>`,
+    "date": "2020-10-31",
   },
-  { title: `<a href="/OPUS-100">OPUS-100 corpus</a>`, date: "2020-06-30" },
-  { title: `<a href="/ELRCpublic">ELRC public</a>`, date: "2020-05-22" },
+  { "title": `<a href="/OPUS-100">OPUS-100 corpus</a>`, date: "2020-06-30" },
+  { "title": `<a href="/ELRCpublic">ELRC public</a>`, date: "2020-05-22" },
   {
-    title: `<a href="/MultiParaCrawl/corpus/version/MultiParaCrawl">MultiParaCrawl</a>`,
-    date: "2019-10-16",
-  },
-  {
-    title: `<a href="/Infopankki/corpus/version/Infopankki">Infopankki v1</a>`,
-    date: "2019-10-14",
+    "title": `<a href="/MultiParaCrawl/corpus/version/MultiParaCrawl">MultiParaCrawl</a>`,
+    "date": "2019-10-16",
   },
   {
-    title: `New corpus: <a href="/memat/corpus/version/memat">memat</a> (Xhosa/English)`,
-    date: "2018-10-06",
+    "title": `<a href="/Infopankki/corpus/version/Infopankki">Infopankki v1</a>`,
+    "date": "2019-10-14",
   },
   {
-    title: `New corpora: <a href="/ParaCrawl/corpus/version/ParaCrawl">ParaCrawl</a>, <a href="/XhosaNavy/corpus/version/XhosaNavy">XhosaNavy</a>`,
-    date: "2018-02-15",
+    "title": `New corpus: <a href="/memat/corpus/version/memat">memat</a> (Xhosa/English)`,
+    "date": "2018-10-06",
   },
   {
-    title: `New version: <a href="/ParaCrawl/corpus/version/ParaCrawl">OpenSubtitles2018</a>`,
-    date: "2017-11-06",
-  }]
+    "title": `New corpora: <a href="/ParaCrawl/corpus/version/ParaCrawl">ParaCrawl</a>, <a href="/XhosaNavy/corpus/version/XhosaNavy">XhosaNavy</a>`,
+    "date": "2018-02-15",
+  },
+  {
+    "title": `New version: <a href="/ParaCrawl/corpus/version/ParaCrawl">OpenSubtitles2018</a>`,
+    "date": "2017-11-06",
+  }}
 
